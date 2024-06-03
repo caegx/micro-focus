fdae966e650903814267c1d80364449706c2e170
Bernard Essilfie <essilfiebernard85@gmail.com>
2024-06-03 12:49:06 +0000

    Last update

--- microfocusdjango/admin.py
+++ microfocusdjango/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import AccessKey, CustomUser
+from .models import AccessKey, CustomUser, Payment
 
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(AccessKey)
+admin.site.register(Payment)
+
+
+#The following environment is selected: ~\Desktop\micro-focus\.venv\Scripts\python.exe
